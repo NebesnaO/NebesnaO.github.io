3a4457bd7a9f79db8e179780865996c32b47e7db
nebesna <nebesna.olga@gmail.com>
2016-04-13 13:56:45 +0000

    first commit lesson1

---
+++ markup/homejs/lesson1/js/script.js
@@ -0,0 +1,15 @@
+var base = prompt('Введите число, которое будет возводиться в степень');
+var exponent = prompt('Введите степень, которое надо применить к числу');
+
+function pow(base, exponent) {
+    var result = base;
+
+    for (var i = 1; i < exponent; i++) {
+        result *= base;
+    }
+return result;
+
+}
+var result = pow(base,exponent);
+console.log('Результат = ',  result);
+
